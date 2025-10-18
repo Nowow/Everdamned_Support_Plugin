7ea70d02ecf0243b18a5c7ca06f9d5089836b072
Nowow <animexones@gmail.com>
2025-10-18 23:03:19 +0400

    better

--- src/bloodmeter.cpp
+++ src/bloodmeter.cpp
@@ -5,6 +5,10 @@
 namespace logger = SKSE::log;
 
 RE::BSString current_widget_root = "_root.WidgetContainer.7.widget";
+RE::BSString root_setpercent = "_root.WidgetContainer.7.widget.setPercent";
+RE::BSString root_fadeto = "_root.WidgetContainer.7.widget.fadeTo";
+RE::BSString root_setalpha = "_root.WidgetContainer.7.widget.setAlpha";
+
 constexpr std::string_view bloodPoolAVname = "ED_BloodPool";
 RE::ActorValue bloodPoolAV;
 RE::TESGlobal* totalBloodPoolGlobal;
@@ -16,14 +20,37 @@ bool bloodMeterIsUpdating = false;
 std::thread bloodMeterUpdateThread;
 
 
+void sendModEvent(RE::TESForm* sender, std::string eventName, std::string strArg, float numArg) {
+    SKSE::ModCallbackEvent modEvent{eventName, strArg, numArg, sender};
+
+    auto modCallbackSource = SKSE::GetModCallbackEventSource();
+    modCallbackSource->SendEvent(&modEvent);
+}
+
+
 void CommunicateCurrentWidgetRoot(RE::StaticFunctionTag*, RE::BSString widget_root) {
     
     if (widget_root.empty()) {
         logger::debug("Widget root communicated was empty");
     }
-        
+     
     current_widget_root = widget_root;
 
+    std::string temp = widget_root.c_str();
+    temp += ".setPercent";
+    logger::debug("Widget .setPercent root: {}", temp);
+    root_setpercent = temp.c_str();
+
+    temp = widget_root.c_str();
+    temp += ".fadeTo";
+    logger::debug("Widget .fadeTo root: {}", temp);
+    root_fadeto = temp.c_str();
+
+    temp = widget_root.c_str();
+    temp += ".setAlpha";
+    logger::debug("Widget .setAlpha root: {}", temp);
+    root_setalpha = temp.c_str();
+    
 }
 
 RE::ActorValue GetBloodPoolAV() {
@@ -48,7 +75,7 @@ RE::ActorValue GetBloodPoolAV() {
 
 void SetBloodMeterPercent(bool bForce) {
 
-    if (current_widget_root.empty()) {
+    if (root_setpercent.empty()) {
         logger::debug("Current meter root not defined, doing nothing");
         return;
     }
@@ -71,10 +98,16 @@ void SetBloodMeterPercent(bool bForce) {
     RE::GPtr<RE::GFxMovieView> view = hud->uiMovie;
 
     RE::GFxValue args[2];
-    args[0].SetNumber(currentRatio);
-    args[1].SetNumber(0.0f);
+    args[0].SetNumber(currentRatio);  //percent to display
+    args[1].SetNumber(0.0f);  //bool if fade or instant
     
-    view->Invoke(current_widget_root.c_str(), nullptr, args, 1);
+    view->Invoke(root_setpercent.c_str(), nullptr, args, 1);
+
+    RE::GFxValue args2[2];
+    args2[0].SetNumber(100.f);  // full alpha
+    args2[1].SetNumber(0.1f);  // duration over which to fade
+
+    view->Invoke(root_fadeto.c_str(), nullptr, args2, 2);
 
 }
 
